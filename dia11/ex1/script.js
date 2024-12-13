35032885cca0766c5463ca3e791f00615264e5dc
Garbelin3 <jp.garbeline@gmail.com>
2024-12-13 19:22:31 -0400

    add ex dia 11

--- dia11/ex1/script.js
+++ dia11/ex1/script.js
@@ -2,9 +2,9 @@ let continuar = true;
 let alunos = [];
 let notas = [];
 let contador = 0;
-do {
+while (continuar){
     let nome = prompt(`Digite o nome do ${contador + 1}° aluno:`) //contador++?
-    let nota = prompt(`Digite a nota do aluno`)
+    let nota = parseInt(prompt(`Digite a nota do aluno`))
     alunos[contador] = nome
     notas[contador] = nota
     contador++ // --
@@ -13,6 +13,18 @@ do {
         continuar = false;
         alert("Obrigado!")
     }
+}
 
+console.log("As notas dos alunos:")
+for (let i = 0; i < alunos.length; i++){
+    console.log(alunos[i] + " - " + notas[i])
+}
 
-} while (continuar)
+let soma = 0
+for (let i = 0; i < notas.length; i++){
+    soma += notas[i]
+}
+
+let media = soma / alunos.length
+console.log("A soma das notas foi de: " + soma)
+console.log("A média geral da turma foi: " + media.toFixed(2))
